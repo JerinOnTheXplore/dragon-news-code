d41a1d0b6981e67fe38e4aab5010c7662211b900
JerinXplore <jerinjerin101325@gmail.com>
2025-05-01 08:02:55 +0600

    implemented marquee and designed navbar

--- src/Components/LatestNews.jsx
+++ src/Components/LatestNews.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Marquee from 'react-fast-marquee';
 
 const LatestNews = () => {
     return (
-        <div>
-            <h2>Latest</h2>
+        <div className='flex items-center gap-5 bg-base-200 p-3'>
+           <p className='text-base-100 bg-secondary px-3 py-2 font-bold'>Latest</p>
+           <Marquee className='flex gap-5'pauseOnHover={true} speed={60}><p>dvffghh Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, ratione impedit reiciendis magnam earum dignissimos velit. Corrupti iure tenetur inventore nobis libero dicta sapiente consequatur ad necessitatibus incidunt, repellat veritatis?</p></Marquee>
+           
         </div>
     );
 };
