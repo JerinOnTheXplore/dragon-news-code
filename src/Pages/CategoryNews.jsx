57d86db073d60fbfc51fdefd23f5b01daf1fee7a
JerinXplore <jerinjerin101325@gmail.com>
2025-05-01 11:02:10 +0600

    loaded news data and filtered news data based on category

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,12 +1,31 @@
-import React, { use } from 'react';
-import { useParams } from 'react-router';
+import React, { use, useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
     const {id}=useParams();
-    console.log(id);
+    const data =useLoaderData();
+    const [CategoryNews,setCategoryNews]=useState([]);
+    // console.log(id,data);
+    useEffect(()=>{
+        if(id=="0"){
+            setCategoryNews(data);
+            return;
+        } 
+        else if (id=="1"){
+            const filteredNews=data.filter((news)=>news.others.is_today_pick==true);
+            setCategoryNews(filteredNews);
+        }
+        else{
+            const filteredNews=data.filter((news)=>news.
+        category_id==id);
+        console.log(filteredNews)
+        setCategoryNews(filteredNews);
+        }
+        
+    },[data,id]);
     return (
         <div>
-            <h2>category news-{id}</h2>
+            Total {CategoryNews.length} News Found
         </div>
     );
 };
